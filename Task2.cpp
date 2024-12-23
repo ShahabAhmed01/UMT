a3a1ebf76433514b0f895f6e0cfce45ab4867a43
Shahab Ahmed <paranoidhere01@gmail.com>
2024-12-23 17:29:52 +0500

    Prompt the user to input 5 values and display the minimum number amongst  them.

--- Task2.cpp
+++ Task2.cpp
@@ -1,14 +1,24 @@
 
 #include <iostream>
 using namespace std;
-
 int main() {
-  int num;
-  cout << "Enter an integer: ";
-  cin >> num;
-  if ( num % 2 == 0)
-    cout << num << " is even.";
-  else
-    cout << num << " is odd.";
-  return 0;
+    int num1, num2, num3, num4, num5;
+    int mini;
+    cout << "Please enter 5 numbers respectively: ";
+    cin >> num1 >> num2 >> num3 >> num4 >> num5;
+    mini = num1;
+    if (num2 < mini) {
+        mini = num2;
+    }
+    else if (num3 < mini) {
+        mini = num3;
+    }
+    else if (num4 < mini) {
+        mini = num4;
+    }
+    else if (num5 < mini) {
+        mini = num5;
+    }
+    cout << "The minimum number is : " << mini << endl;
+    return 0;
 }
