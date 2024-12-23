d26c9ce870d918945ffbbd9b188a1701d335c002
Shahab Ahmed <paranoidhere01@gmail.com>
2024-12-23 17:58:59 +0500

    Write a C++ program to print the multiplication table of a given  number using a do-while loop

--- Task5.cpp
+++ Task5.cpp
@@ -1,10 +1,18 @@
 
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main(){
-  int i;
-  for (i = 5; i >= 0; i--){
-    cout << " " << i;
-  }
-  return 0;
+int main()
+{
+	int number;
+	int i=1;
+
+	cout<<"enter a number"<<endl;
+	cin>>number;
+
+	do{
+		cout<<number<<"*"<<i<<"="<<number*i<<endl;
+		i++;
+	}while(i<=10);
+	return 0;
+
 }
