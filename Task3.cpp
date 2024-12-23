f97cbe84c2e2ca544fb897bc220caee2277e3ead
Shahab Ahmed <paranoidhere01@gmail.com>
2024-12-23 18:22:50 +0500

    Write a program that calculates the sum of integers entered by the user using a  while loop. If the user enters 0, the program should break out of the loop and  display the total sum. Use the continue statement to skip negative numbers  without adding them to the sum.

--- Task3.cpp
+++ Task3.cpp
@@ -1,19 +1,22 @@
 
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main(){
-  int num, i;
-  cout << "Enter a number";
-  cin >> num;
-  if (num < 0){
-        cout << "Entered number is invalid";
-        return 0;
-  }
-   for (i = 1; i <= num; i++){
-    if (i % 2 != 0){
-    cout << "\t" << i;
-    i = i + 1;
-  }
-   }
-  return 0;
+int main()
+{
+	int num;
+	int sum=0;
+	cout<<"enter the integer values you have to add(Enter zero to end):"<<endl;
+	while(true){
+		cout<<"enter the integer:"<<endl;
+		cin>>num;
+		if(num==0){
+			break;
+		}
+		if(num<0){
+			continue;
+		}
+	sum+=num;
+}
+	cout<<"the sum of integer is"<<sum<<endl;
+	return 0;
 }
