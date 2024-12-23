7ce427b5533b3979ff3148a8db85687e8fc350d1
Shahab Ahmed <paranoidhere01@gmail.com>
2024-12-23 17:58:13 +0500

    Write a C++ program to display numbers from 1 to 10 and each  number displayed on a separate line using do-while Loop.

--- Task4.cpp
+++ Task4.cpp
@@ -1,16 +1,12 @@
 
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main(){
-  int num, i;
-  cout << "Enter a number";
-  cin >> num;
-    if (num < 0){
-        cout << "Entered number is invalid";
-        return 0;
-  }
-  for(i = 1; i <= num; i++){
-    cout << num << " X " << i <<  " = " << num*i << endl;
-  }
-  return 0;
+int main()
+{
+	int i=1;
+	do{
+			cout<<i<<endl;
+			i++;
+	}while(i<=10);
+	return 0;
 }
