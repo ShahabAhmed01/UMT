9e4d72953e819350963aea8bec45d5504d5c5b71
Shahab Ahmed <paranoidhere01@gmail.com>
2024-12-23 17:56:05 +0500

    Write a program in C++ to find the factorial of a number using while loop.

--- Task1.cpp
+++ Task1.cpp
@@ -1,24 +1,25 @@
 
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
-    int number, maxi = 0;
-    cout << "Enter a number:";
-    cin >> number;
-    for (int i = 1; i <= number; i++)
-    {
-        if (number % i == 0)
-        {
-            maxi = maxi + 1;
-        }
-    }
-    if (maxi <= 2){
-        cout << "It is a prime number" ;
-    }
-    else
-     {
-        cout << "It is composite";
-    }
-    return 0;
+	int num;
+	long factorial=1;
+
+	cout<<"Enter a positive number:";
+	cin>>num;
+	if(num < 0)
+	{
+		cout<<"Please enter a positive number"<<endl;
+
+	}
+	else{
+		int i=num;
+		while(i>=1){
+			factorial = factorial*i;
+			i--;
+		}
+			cout<<"the factorial of given number is"<<factorial<<endl;
+	}
+	return 0;
 }
