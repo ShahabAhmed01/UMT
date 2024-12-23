0e70c9c231a16ac8945c3dd698984c702d474cb7
Shahab Ahmed <paranoidhere01@gmail.com>
2024-12-23 17:36:52 +0500

    Prompt the user to input a character and display whether it is a vowel or consonant.

--- Task1.cpp
+++ Task1.cpp
@@ -1,33 +1,16 @@
-
 #include <iostream>
-using namespace std;
+ using namespace std;
 
-int main() {
-    char op;
-    cout << "Please enter a Operator (+, -, *, /)" << endl;
-    cin >> op;
-    cout << "Enter value one" << endl;
-    float num1;
-    cin >> num1;
-    cout << "Enter value two" << endl;
-    float num2;
-    cin >> num2;
-    switch (op){
-        case '+':
-            cout << num1 << " + " << num2 << " = " << num1 + num2 <<endl;
-        break;
-        case '-':
-            cout << num1 << " - " << num2 << " = " << num1 - num2 <<endl;
-        break;
-        case '*':
-            cout << num1 << " * " << num2 << " = " << num1 * num2 <<endl;
-        break;
-        case '/':
-            cout << num1 << " / " << num2 << " = " << num1 / num2 <<endl;
-        break;
-        default:
-            cout << "Enter a valid Operator" << endl;
+int main(){
+    char charac;
+    cout << "Input a character:";
+    cin >> charac;
+    if ( charac == 'a' || charac == 'e' || charac == 'i' || charac == 'o' || charac == 'u'){
+        cout << "Entered character is a vowel";
+    }
+    else{
+        cout << "Entered character is a Consonant";
     }
     return 0;
-}
 
+}
