53540f60bbf86daddcdac838876cb89fa94b0789
Shahab Ahmed <paranoidhere01@gmail.com>
2024-12-23 17:30:41 +0500

    Write a C++ program that uses a switch statement to determine if a number  is even or odd.

--- Task3.cpp
+++ Task3.cpp
@@ -1,29 +1,22 @@
 
+
 #include <iostream>
 using namespace std;
 int main() {
-    int score;
-    char grade;
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    switch (number % 2) {
+        case 0:
+            cout << "The number is even." << endl;
+        break;
+        case 1:
+            cout << "The number is odd." << endl;
+        break;
+        default:
+            cout << "Error" << endl;
 
-    cout << "Please enter the student's score: ";
-    cin >> score;
-
-    if (score >= 90 && score <= 100) {
-        grade = 'A';
     }
-    else if (score >= 80 && score < 90) {
-        grade = 'B';
-    }
-    else if (score >= 70 && score < 80) {
-        grade = 'C';
-    }
-    else if (score >= 60 && score < 70) {
-        grade = 'D';
-    }
-    else {
-        grade = 'F';
-    }
-    cout << "The student's grade is: " << grade << endl;
-
     return 0;
 }
+
