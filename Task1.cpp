3914c67dd7876195b5e98d3752c7ab36ee2696ef
Shahab Ahmed <paranoidhere01@gmail.com>
2024-12-23 17:28:31 +0500

    Create a calculator that performs basic operations (addition, subtraction,  multiplication, and division) based on user input. Use a switch statement to handle  the operation selection.

--- Task1.cpp
+++ Task1.cpp
@@ -1,15 +1,33 @@
 
 #include <iostream>
 using namespace std;
-int main() {
-    int age;
-    cout << "Enter your age: ";
-    cin >> age;
-    if (age >= 18) {
-        cout << "You are eligible to vote." <<endl;
-    } else {
-        cout<< "You are not eligible to vote." <<endl;
-    }
 
+int main() {
+    char op;
+    cout << "Please enter a Operator (+, -, *, /)" << endl;
+    cin >> op;
+    cout << "Enter value one" << endl;
+    float num1;
+    cin >> num1;
+    cout << "Enter value two" << endl;
+    float num2;
+    cin >> num2;
+    switch (op){
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2 <<endl;
+        break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2 <<endl;
+        break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2 <<endl;
+        break;
+        case '/':
+            cout << num1 << " / " << num2 << " = " << num1 / num2 <<endl;
+        break;
+        default:
+            cout << "Enter a valid Operator" << endl;
+    }
     return 0;
 }
+
