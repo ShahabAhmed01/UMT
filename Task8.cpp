69756194fe5ec838ce1909b1b80126f9d284acc5
Shahab Ahmed <paranoidhere01@gmail.com>
2025-01-14 18:19:26 +0500

    total of all students and class average
     A class of ten students took a quiz. The grades (integers in the range 0 to 100) for this 
    quiz are available to you. Calculate and display the total of all student grades and the class 
    average on the quiz.

--- Task8.cpp
+++ Task8.cpp
@@ -1,23 +1,33 @@
-
+/*
+Q8: A class of ten students took a quiz. The grades (integers in the range 0 to 100) for this
+quiz are available to you. Calculate and display the total of all student grades and the class
+average on the quiz.
+*/
 #include <iostream>
 using namespace std;
-int main(){
- int num;
- for (;;){
-        cout << "Enter a number";
-         cin >> num;
-           if (num < 0){
-        cout << "Entered number is invalid";
-        return 0;
-  }
-        if (num % 7 == 0){
-            cout << "Number is divisble by 7 \n";
-            break;
-        }
-        else {
-              cout << "Enter again \n";
-
-              }
- }
-  return 0;
+// Function to calculate the total marks of all students
+int Total(int grades[], int size)
+{
+    int total = 0;
+    for (int i = 0; i < size; i++)
+    {
+        total = total + grades[i];
+    }
+    return total;
+}
+// Function to calculate the average
+double Average(int total, int size)
+{
+    int avg = total / 10; // we can also use total / size
+    return avg;
+}
+int main()
+{
+    int numStudents = 10;
+    int grades[numStudents] = {85, 92, 78, 64, 88, 73, 90, 79, 84, 91};
+    int totalmarks = Total(grades, numStudents);
+    double averagee = Average(totalmarks, numStudents);
+    cout << "Total marks of all students: " << totalmarks << endl;
+    cout << "Average: " << averagee << endl;
+    return 0;
 }
