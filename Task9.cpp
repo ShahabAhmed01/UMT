f1d5340b354c519bdea360beb60e32fabcdf73db
Shahab Ahmed <paranoidhere01@gmail.com>
2025-01-14 18:19:52 +0500

    Write a program to Printing and count Even and Odd Numbers using While Loop in  C++.

--- Task9.cpp
+++ Task9.cpp
@@ -1,17 +1,35 @@
-
+//Q9: Write a program to Printing and count Even and Odd Numbers using While Loop in C++.
 #include <iostream>
+#include <cstdlib> // Included cstdlib for rand() and srand(), these functions are used to random values
+#include <ctime>
 using namespace std;
-int main() {
-    int number, sum = 0;
-    for (int i = 1; i <= 10; i++) {
-        cout << "Enter number: ";
-        cin >> number;
-        if (number < 0) {
-    cout << "Negative number is entered" << endl;
-        continue;
-        }
-        sum = sum + number;
+int main()
+{
+    srand(static_cast<unsigned int>(time(0))); //random number generator with current time
+    int countEven = 0, countOdd = 0;
+    int arrayy[50];
+    int i;
+    for (i = 0; i <= 49; i++)
+    {
+        arrayy[i] = rand() % 50 + 1; // rand() generates random number between 1 and 50 and store it in array
     }
-    cout << "The sum of all positive numbers is: " << sum << endl;
+    for (i = 0; i <= 49; i++)
+    {
+        if (arrayy[i] % 2 == 0)
+        {
+            countEven++;
+        }
+        else
+        {
+            countOdd++;
+        }
+    }
+    for (i = 0; i <= 49; i++)
+    {
+        cout << arrayy[i] << " ";
+    }
+    cout << endl;
+    cout << "Even Numbers: " << countEven << endl;
+    cout << "Odd Numbers: " << countOdd << endl;
     return 0;
 }
