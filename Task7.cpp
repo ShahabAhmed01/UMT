80aa9fe97da1d1b9a0d4bbaadf7310eaede7c729
Shahab Ahmed <paranoidhere01@gmail.com>
2025-01-14 18:18:18 +0500

    Write a program in C++ to find the factorial of a number.

--- Task7.cpp
+++ Task7.cpp
@@ -1,12 +1,21 @@
-
 #include <iostream>
 using namespace std;
-int main(){
-  int i;
-  for (i = 1; i <= 50; i++){
-    if (i % 2 == 0){
-        cout << " " << i
+int fact(int x)
+{
+    int fact = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        fact = fact * i;
     }
-  }
-  return 0;
+    return fact;
+}
+int main()
+{
+    int num;
+    int ans;
+    cout << "Please enter a number: ";
+    cin >> num;
+    ans = fact(num);
+    cout << "Factorial of " << num << " is: " << ans << endl;
+    return 0;
 }
